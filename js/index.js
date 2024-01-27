47cc6996258c27545f5f38d7b0d2ed62159b3aa0
Z1n <tv.noob@bk.ru>
2024-01-27 21:28:27 +0700

    test

---
+++ js/index.js
@@ -0,0 +1,26 @@
+import axios from '../node_modules/axios';
+
+const url = 'http://localhost:63342/form-generator/data/form-test-1.json';
+
+// axios.get('http://localhost:63342/form-generator/data/form-test-1.json')
+//     .then(function (response) {
+//         // обработка успешного запроса
+//         console.log(response);
+//     })
+//     .catch(function (error) {
+//         // обработка ошибки
+//         console.log(error);
+//     })
+//     .finally(function () {
+//         // выполняется всегда
+//         console.log('work axios')
+//     });
+//
+// // fetch(url)
+// //     .then((response) => {
+// //         return response.json();
+// //     })
+// //     .then((data) => {
+// //         console.log(data);
+// //     });
+console.log("work")
